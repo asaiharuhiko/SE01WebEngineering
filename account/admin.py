6128ba16fa4f48cba1e6bc5b6d0e13d095eee802
asaiharuhiko <0120mmasai@gmail.com>
2026-07-13 12:30:45 +0900

    add app account and add spec.md
    app account is not enough to use it, and spec.md installed because of we need to think about specification.

--- account/admin.py
+++ account/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from account.models import user_account
 
 # Register your models here.
-    
+admin.site.register(user_account)
--- account/models.py
+++ account/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class user_account(models.Model):
+    user_id = models.CharField(max_length=20,unique=True)
+    password = models.TextField()
+    
+    def __str__(self):
+        return self.user_id
