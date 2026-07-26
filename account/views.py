53054b890bb11af5af20718f6fcbab806ceac369
asaiharuhiko <0120mmasai@gmail.com>
2026-07-27 02:20:13 +0900

    #3 basic view

--- account/views.py
+++ account/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import View
+from django.shortcuts import redirect
+from django.http import HttpResponse
 
-# Create your views here.
+
+class IndexView(View):
+    def get(self,request):
+            return render(request,"post/index.html")
+        
+index = IndexView.as_view()
