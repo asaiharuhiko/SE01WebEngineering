3bdb25ce41f99defea292bed2eb74649586a1ec0
asaiharuhiko <0120mmasai@gmail.com>
2026-07-28 16:42:36 +0900

    refine some code
    refrence : https://github.com/asaiharuhiko/SE01WebEngineering/pull/10#issuecomment-5101114830

--- post/calendars.py
+++ post/calendars.py
@@ -1,5 +1,6 @@
 import calendar 
 from datetime import date
+from django.urls import reverse
 
 class BlogCalendar(calendar.HTMLCalendar):
     def __init__(self, year, month):
@@ -12,10 +13,23 @@ class BlogCalendar(calendar.HTMLCalendar):
             return '<td class="noday">&nbsp;</td>'
 
         d = date(self.year, self.month, day)
-        url = f'/post/search/?date={d:%Y-%m-%d}'
+        url = reverse("post:search") + f"?date{d:%Y-%m-%d}"
         return (
             f'<td class="day">'
             f'<a href="{url}"'
             f'>{day}</a>'
             f'</td>'
         )
+
+
+def get_prev_next_month(year, month):
+    if month == 1:
+        prev_year, prev_month = year - 1, 12
+        next_year, next_month = year, 2
+    elif month == 12:
+        prev_year, prev_month = year, 11
+        next_year, next_month = year + 1, 1
+    else:
+        prev_year, prev_month = year, month - 1
+        next_year, next_month = year, month + 1
+    return prev_year, prev_month, next_year, next_month
